3437a8b1f7eccdad5e49a24732a689c56a1fdf67
osman mohammed <uniqueginun@gmail.com>
2022-01-12 22:45:13 +0300

    successfully install inertia with Vue3

---
+++ resources/js/Views/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+   welcome to world
+   <h4>{{ message }}</h4>
+</template>
+
+<script>
+export default {
+   setup() {
+      return {
+         message: 'hello world'
+      }
+   },
+}
+</script>
